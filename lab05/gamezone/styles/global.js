fa5aa23999811f0c6e8e5ef9fcff45c13dcac8b6
nstrain <nstrain15@gmail.com>
2020-10-01 14:38:50 -0400

    lab05

---
+++ lab05/gamezone/styles/global.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  card: {
+    borderRadius: 6,
+    elevation: 3,
+    backgroundColor: '#fff',
+    shadowOffset: {width: 1, height: 1},
+    shadowColor: '#333',
+    shadowRadius: 2,
+    marginHorizontal: 4,
+    marginVertical: 6
+  },
+  cardContent: {
+    marginHorizontal: 18,
+    marginVertical: 10,
+  },
+  header: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#333',
+    letterSpacing: 1,
+  },
+  icon: {
+    position: 'absolute',
+    left: 16,
+  }
+});
